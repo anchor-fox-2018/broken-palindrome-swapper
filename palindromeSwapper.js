7e454f42c13da84e8960ddc531960b17a32d91fb
days88 <yayahidayatullah@rocketmail.com>
2018-09-04 13:04:16 +0800

    Broken Palindrome Swapper

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -1,4 +1,4 @@
-/**
+/*
  * @function palindromeSwapper
  * menukar 2 huruf berurutan dari n = 0 sampai n-1
  * contoh: makan => amkan, mkaan, maakn, makna
@@ -8,29 +8,41 @@
 function palindromeSwapper(str) {
   let newStr = '';
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j+1];
         newStr += str[j];
-        j++;
+        j++
+        //console.log(newStr)
       } else {
         newStr += str[j];
+        //console.log('====' , newStr)
       }
-    }
-    if (isPalindrome(newStr)) return true;
-  }
-  return false;
-}
 
-/**
+    }
+    //console.log(newStr)
+    if (isPalindrome(newStr) === true||isPalindrome(str)=== true){
+      return true;
+    }else{
+      return false;
+    }
+    
+  }
+  
+}
+/*
  * @function isPalindrome
  * Cek apabila kata merupakan palindromeSwapper
  * @param {string} str - input kata yang dicek bila palindrom
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  var strBaru =str.split('').reverse().join('')
+  if(strBaru === str){
+    return true
+  }else{
+    return false
+  }
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
